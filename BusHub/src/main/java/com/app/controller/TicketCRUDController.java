5874227333fdbbcc7b455ab54796e151a5b2f2de
vikramv29 <vikramvermavv24@gmail.com>
2021-09-28 15:08:05 +0530

    JSON and other params updated.

--- BusHub/src/main/java/com/app/controller/TicketCRUDController.java
+++ BusHub/src/main/java/com/app/controller/TicketCRUDController.java
@@ -1,6 +1,7 @@
 package com.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import com.app.service.TicketCRUDService;
 import com.app.service.impl.TicketCRUDServiceImpl;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class TicketCRUDController {
 
 	@Autowired
